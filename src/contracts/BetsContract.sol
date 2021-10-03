28c6a99428f876c969ffc84e564e256bbf62b43b
lucas-naman <lucas.naman@gmail.com>
2021-10-03 19:07:39 +0200

    BetsContract updated lists into mapping

--- src/contracts/BetsContract.sol
+++ src/contracts/BetsContract.sol
@@ -15,17 +15,20 @@ contract BetsContract {
         uint256 amountA;
         uint256 amountB;
         uint256 timeBetClose;
-        Gamble[] gambles;
+        mapping(uint256 => Gamble) gambles;
+        uint256 nbGambles;
     }
 
     string public name = "DApp Bets";
     TreatsToken public treatsToken;
     address public owner;
-    Bet[] public bets;
+    mapping(uint256 => Bet) public bets;
+    uint256 nbBets;
 
     constructor(TreatsToken _treatsToken) public {
         treatsToken = _treatsToken;
         owner = msg.sender;
+        nbBets = 0;
     }
 
     modifier onlyOwner() {
@@ -47,7 +50,8 @@ contract BetsContract {
         bet.teamA = _teamA;
         bet.teamB = _teamB;
         bet.timeBetClose = _timeBetClose;
-        bets.push(bet);
+        bets[nbBets] = bet;
+        nbBets += 1;
     }
 
     function participate(
@@ -62,11 +66,12 @@ contract BetsContract {
             amount: _amount,
             gambler: msg.sender
         });
-        bets[_idx].gambles.push(gamble);
+        bets[_idx].gambles[bets[_idx].nbGambles] = gamble;
+        bets[_idx].nbGambles += 1;
     }
 
     function setBetWinner(uint256 _idx, bool _winner) public onlyOwner {
-        for (uint256 i = 0; i < bets[_idx].gambles.length; i++) {
+        for (uint256 i = 0; i < bets[_idx].nbGambles; i++) {
             if (_winner == bets[_idx].gambles[i].team) {
                 uint256 amount = bets[_idx].gambles[i].amount;
                 if (_winner) {
