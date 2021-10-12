7abdcf26a63b5723c60967fef98ba3c2ce5a7997
lucas-naman <lucas.naman@gmail.com>
2021-10-12 16:28:30 +0200

    updated migration for ERC20

---
+++ scripts/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+const TreatsToken = artifacts.require('TreatsToken')
+const BetsContract = artifacts.require('BetsContract')
+
+module.exports = async function(deployer, network, accounts) {
+  await deployer.deploy(TreatsToken)
+  const treatsToken = await TreatsToken.deployed()
+
+  await deployer.deploy(BetsContract, treatsToken.address)
+  const betsContract = await BetsContract.deployed()
+
+  //await treatsToken.transfer(accounts[0], '1000000000000000000000000000')
+};
